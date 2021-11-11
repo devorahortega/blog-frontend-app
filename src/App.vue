17d983672cfb6567493c7d162c21d4998bed05e3
Devorah Ortega <devorahb.ortega@gmail.com>
2021-11-11 13:59:15 -0500

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,14 @@
       <router-link to="/">Home</router-link>
       |
       <router-link to="/about">About</router-link>
+      |
+      <router-link to="/test">Test</router-link>
+      |
+      <router-link to="/signup">Signup</router-link>
+      |
+      <router-link to="/login">Login</router-link>
+      |
+      <router-link to="/logout">Logout</router-link>
     </div>
     <router-view />
   </div>
@@ -16,6 +24,12 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+}
+
+body {
+  text-align: center;
+  border: 12px solid;
+  border-image: linear-gradient(135deg, #2c3e50 0%, #4d6c8b 25%, #fcfcfc 50%, #42b983 75%, #23694a 100%) 1;
 }
 
 #nav {
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,14 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
-
+import Test from "../views/Test.vue";
+import Signup from "../views/Signup.vue";
+import Login from "../views/Login.vue";
+import Logout from "../views/Logout.vue";
 Vue.use(VueRouter);
 
 const routes = [
-  {
-    path: "/",
-    name: "Home",
-    component: Home,
-  },
+  { path: "/", name: "Home", component: Home },
   {
     path: "/about",
     name: "About",
@@ -18,6 +17,10 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ "../views/About.vue"),
   },
+  { path: "/test", name: "Test", component: Test },
+  { path: "/signup", name: "signup", component: Signup },
+  { path: "/login", name: "login", component: Login },
+  { path: "/logout", name: "logout", component: Logout },
 ];
 
 const router = new VueRouter({
