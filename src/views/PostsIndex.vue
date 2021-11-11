c6a9e3c2c8a409381b229ce6c6b5666982a2aedd
Devorah Ortega <devorahb.ortega@gmail.com>
2021-11-11 14:43:15 -0500

    Adds an index page to website

---
+++ src/views/PostsIndex.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="postindex">
+    <h1>{{ message }}</h1>
+    <div v-for="post in posts" :key="post.id">
+      <h2>{{ post.title }}</h2>
+      <img :src="post.image_url" :alt="post.title" />
+      <p>{{ post.body }}</p>
+    </div>
+  </div>
+</template>
+;
+<style></style>
+
+<script>
+import axios from "axios";
+export default {
+  data: function () {
+    return {
+      message: "Posts:",
+      posts: [],
+    };
+  },
+  created: function () {
+    this.indexPosts();
+  },
+  methods: {
+    indexPosts: function () {
+      axios.get("http://localhost:3000/posts").then((response) => {
+        console.log("Posts", response.data);
+        this.posts = response.data;
+      });
+    },
+  },
+};
+</script>
+;
