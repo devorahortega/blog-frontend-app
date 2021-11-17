140d3b42648b030d566186a921b98b9c468e1d34
Devorah Ortega <devorahb.ortega@gmail.com>
2021-11-17 13:27:39 -0500

    Adds selected component to PostsIndex

--- src/views/PostsIndex.vue
+++ src/views/PostsIndex.vue
@@ -2,16 +2,29 @@
   <div class="postindex">
     <h1>{{ message }}</h1>
     <div v-for="post in posts" :key="post.id">
-      <router-link v-bind:to="`/posts/${post.id}`">
-        <h2>{{ post.title }}</h2>
-      </router-link>
-      <img :src="post.image_url" :alt="post.title" />
-      <p>{{ post.body }}</p>
+      <div
+        v-for="post in posts"
+        v-on:click="currentPost = post"
+        v-bind:class="{ selected: post === currentPost }"
+        :key="post.id"
+      >
+        <router-link v-bind:to="`/posts/${post.id}`">
+          <h2>{{ post.title }}</h2>
+        </router-link>
+        <img :src="post.image_url" :alt="post.title" />
+        <p>{{ post.body }}</p>
+      </div>
     </div>
   </div>
 </template>
 ;
-<style></style>
+<style>
+.selected {
+  color: cadetblue;
+  background-color: cyan;
+  transition: background-color 2s ease;
+}
+</style>
 
 <script>
 import axios from "axios";
@@ -20,6 +33,7 @@ export default {
     return {
       message: "Posts:",
       posts: [],
+      currentPost: {},
     };
   },
   created: function () {
